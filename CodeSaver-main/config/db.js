ff243386d12f3404e4cc4cec9e0a7018e912b617
Md Mujib Alam <56786464+mdmujibalam@users.noreply.github.com>
2022-12-10 00:36:40 +0530

    Update db.js

--- CodeSaver-main/config/db.js
+++ CodeSaver-main/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const config=require('config');
 const connectDB = async() => {
-    await mongoose.connect(config.get('MONGO_URI'),{
+    await mongoose.connect(config.get('http://localhost:27017'),{
         useNewUrlParser:true,
         useCreateIndex:true,
         useUnifiedTopology:true,
